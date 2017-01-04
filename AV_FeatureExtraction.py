c4c2a15065dc139f63b052e85f51586f02ffa66a
morales <mmorales@gradcenter.cuny.edu>
2017-01-04 16:19:06 -0500

    edited AV feat extraction script to include covarep info

--- AV_FeatureExtraction.py
+++ AV_FeatureExtraction.py
@@ -3,9 +3,16 @@
 
 #This script can be used to perform AV (audiovisual) feature extraction using the OpenFace and COVAREP repos
 
+### OpenFace ###
 #Feature extraction with OpenFace
 #ToDo
 #function that takes video as input
 #creates new csv for output
 #runs feature extraction: OpenFace/bin/FeatureExtraction -f /path/to/mov -of /path/to/.csv
 
+### COVAREP ###
+#Feature extraction with covarep
+#ToDo
+#figure out how to call matlab functions in python
+#function that takes audio as input and creates csv and writes features to
+#covarep/feature_extraction/COVAREP_feature_extraction.m
