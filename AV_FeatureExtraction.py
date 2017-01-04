dc20880b9c39cb09fb12db3bdf8eaba69955caf6
morales <mmorales@gradcenter.cuny.edu>
2017-01-04 16:04:12 -0500

    add av feat extraction script

---
+++ AV_FeatureExtraction.py
@@ -0,0 +1,11 @@
+#Michelle Morales
+#Dissertation Work 2017
+
+#This script can be used to perform AV (audiovisual) feature extraction using the OpenFace and COVAREP repos
+
+#Feature extraction with OpenFace
+#ToDo
+#function that takes video as input
+#creates new csv for output
+#runs feature extraction: OpenFace/bin/FeatureExtraction -f /path/to/mov -of /path/to/.csv
+
