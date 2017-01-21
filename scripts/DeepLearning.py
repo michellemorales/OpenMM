048bdb4acf410a0cdc79acf25b147664eded9da8
morales <mmorales@gradcenter.cuny.edu>
2017-01-21 12:18:35 -0500

    changes to DL script

--- scripts/DeepLearning.py
+++ scripts/DeepLearning.py
@@ -1,2 +1,11 @@
 #Michelle Morales
-#
+#Experiments with nueral networks for depression detection
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.optimizers import SGD
+
+model = Sequential() #initialize models
+model.add(Dense(100, input_dim=?)) #add first hidden layer
+model.add()
