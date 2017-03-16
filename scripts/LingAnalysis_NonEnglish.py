c44c2b4f88667a487416fed6d1eecec4ae065870
morales <mmorales@gradcenter.cuny.edu>
2017-03-16 15:37:59 -0400

    added german/spanish parser

--- scripts/LingAnalysis_NonEnglish.py
+++ scripts/LingAnalysis_NonEnglish.py
@@ -28,7 +28,46 @@ def bag_of_words(dir):
     bag = sorted(bag)
     return bag
 
-def get_feats(file_name, bag):
+def spanish_parse(transcript):
+    os.chdir(r"/Users/morales/Github/models/syntaxnet")
+    command = "echo '%s' | syntaxnet/models/parsey_universal/parse.sh /Users/morales/GitHub/models/Spanish" %transcript
+    try:
+        output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
+    except:
+        output = False
+    return output
+def german_parse(transcript):
+    os.chdir(r"/Users/morales/Github/models/syntaxnet")
+    command = "echo '%s' | syntaxnet/models/parsey_universal/parse.sh /Users/morales/GitHub/models/German" %transcript
+    try:
+        output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
+    except:
+        output = False
+    return output
+def dependency_distance(conll_df):
+    """ Computes dependency distance for dependency tree. Based off of:
+    Pakhomov, Serguei, et al. "Computerized assessment of syntactic complexity
+    in Alzheimers disease: a case study of Iris Murdochs writing."
+    Behavior research methods 43.1 (2011): 136-144."""
+    ID = numpy.array([int(x) for x in conll_df['ID']])
+    HEAD = numpy.array([int(x) for x in conll_df['HEAD']])
+    diff = abs(ID-HEAD)
+    total_distance = sum(diff)
+    return total_distance
+def load_tags():
+    #Load universal POS tag set - http://universaldependencies.org/u/pos/all.html
+    with open('/Users/morales/GitHub/OpenMM/data/UniversalPOSTagList.txt','r') as f:
+        tags = [line.strip() for line in f.readlines()]
+    return tags
+def tag_count(df):
+    tag_count = []
+    tags = load_tags()
+    for tag in sorted(tags):
+        df_tags = df['UPOS'].values.tolist()
+        count = df_tags.count(tag)
+        tag_count.append(count)
+    return tag_count
+def get_feats(file_name, bag, lang):
     with open(os.path.join(dir,file_name)) as data_file:
         data = json.load(data_file)
     transcription = []
@@ -39,8 +78,9 @@ def get_feats(file_name, bag):
                 transcription.append(hypothesis["transcript"])
 
     openF = open(file_name.replace('_transcript.json','_ling.csv'),'w')
-    header = ','.join(bag).encode('ascii','ignore')
-    openF.write(header+'\n')
+    bag_header = ','.join(bag).encode('ascii','ignore')
+    syntax_header = 'word_count,avg_wordlen,levels,distance,univ_tag,%s' %(','.join(load_tags()))
+    openF.write(bag_header+','+syntax_header+'\n')
     feature_list = []
     for sentence in transcription:
         print sentence
@@ -51,8 +91,26 @@ def get_feats(file_name, bag):
             for w in bag:
                 count = words.count(w)
                 feats.append(float(count)/word_count)
+            if lang =='German':
+                conll = german_parse(sentence)
+            elif lang == 'Spanish':
+                conll = spanish_parse(sentence)
+            if conll:
+                conll_lines = conll.strip().split('\n')
+                conll_table = [line.split('\t') for line in conll_lines]
+                df = pandas.DataFrame(conll_table,columns=['ID','FORM','LEMMA','UPOS','XPOS','FEATS','HEAD','DEPREL','DEPS','MISC'])
+                pos_feats = tag_count(df) #pos tag count
+                univ_tag = len(set(df['UPOS'].values)) #unique number of pos tags
+                distance = dependency_distance(df) #dependency distance
+                heads = df['HEAD'].values
+                levels = len(set(heads)) #tree depth
+                avg_wordlen = sum([len(w) for w in words])/len(words) #average word length
+                syntax_feats = [word_count, avg_wordlen,levels,distance,univ_tag] + pos_feats
+            else:
+                syntax_feats = 22 * [0]
         else:
             feats = len(bag) * [0]
+        feats = feats + syntax_feats
         features = ','.join([str(f) for f in feats]).encode('ascii','ignore')
         feature_list.append(features)
     for s in feature_list:
