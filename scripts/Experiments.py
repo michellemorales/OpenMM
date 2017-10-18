de188749359d2c0ec38961ee99f14134b46b9bd0
morales <mmorales@gradcenter.cuny.edu>
2017-10-18 07:57:20 -0400

    add in code to run experiments for informed early fusion

--- scripts/Experiments.py
+++ scripts/Experiments.py
@@ -1,5 +1,6 @@
 import Fusions
 import pandas
+import os
 
 
 def get_earlyfusion():
@@ -67,102 +68,172 @@ def get_earlyfusion():
 
 def run_experiments():
     print('\nRunning SVM experiments...\n')
+    # path = "/Users/michellemorales/Desktop/MoralesDocs/DAIC_WOZ/"
+    path = "/Users/morales/Desktop/Dissertation/"
     # Get training labels
-    train_csv = '/Users/michellemorales/Desktop/MoralesDocs/DAIC_WOZ/Labels/training_split.csv'
-    train_df = pandas.read_csv(train_csv)
-    train_labels = {}
-    for row in train_df.iterrows():
-        row = row[1]
-        train_labels[str(row['Participant_ID'])] = {'binary': row['PHQ_Binary'],
-                                                    'score': row['PHQ_Score'], 'gender': row['Gender']}
+
+    # train_csv = path+'Labels/training_split.csv'
+    # train_df = pandas.read_csv(train_csv)
+    # train_labels = {}
+    # for row in train_df.iterrows():
+    #     row = row[1]
+    #     train_labels[str(row['Participant_ID'])] = {'binary': row['PHQ_Binary'],
+    #                                                 'score': row['PHQ_Score'], 'gender': row['Gender']}
 
     # Get dev labels
-    dev_csv = '/Users/michellemorales/Desktop/MoralesDocs/DAIC_WOZ/Labels/dev_split.csv'
-    dev_df = pandas.read_csv(dev_csv)
-    dev_labels = {}
-    for row in dev_df.iterrows():
-        row = row[1]
-        dev_labels[str(row['Participant_ID'])] = {'binary': row['PHQ_Binary'],
-                                                  'score': row['PHQ_Score'], 'gender': row['Gender']}
-    print("Done loading data labels...\n")
+    # dev_csv = path+'Labels/dev_split.csv'
+    # dev_df = pandas.read_csv(dev_csv)
+    # dev_labels = {}
+    # for row in dev_df.iterrows():
+    #     row = row[1]
+    #     dev_labels[str(row['Participant_ID'])] = {'binary': row['PHQ_Binary'],
+    #                                               'score': row['PHQ_Score'], 'gender': row['Gender']}
+    # print("Done loading data labels...\n")
 
     # Load features for train and dev
-    path = "/Users/michellemorales/Desktop/MoralesDocs/DAIC_WOZ/Early_Features/"
-    train_data = pandas.read_csv(path+'train_multimodal_features.csv')
-    audio_names =  train_data.columns[1:371]
-    video_names = train_data.columns[371: 556]
-    text_names = train_data.columns[556:]
-    dev_data =  pandas.read_csv(path+'dev_multimodal_features.csv')
-    print('Done loading data...\n')
+    train_data = pandas.read_csv(path+'Early_Features/train_multimodal_features.csv')
+    dev_data =  pandas.read_csv(path+'Early_Features/dev_multimodal_features.csv')
+    # print('Done loading data...\n')
 
     # Get training data & labels for features
-    t_labels = []
-    t_bin_labels = []
-    t_features = []
-    for row in train_data.iterrows():
-        part_id = str(int(row[1][0]))
-        features = row[1][1:].values
-        t_features.append(features)
-        label = train_labels[part_id]['score']
-        b_label = train_labels[part_id]['binary']
-        t_labels.append(label)
-        t_bin_labels.append(b_label)
+    # t_labels = []
+    # t_bin_labels = []
+    # t_features = []
+    # for row in train_data.iterrows():
+    #     part_id = str(int(row[1][0]))
+    #     features = row[1][1:].values
+    #     t_features.append(features)
+    #     label = train_labels[part_id]['score']
+    #     b_label = train_labels[part_id]['binary']
+    #     t_labels.append(label)
+    #     t_bin_labels.append(b_label)
 
 
     # Get dev data & labels for features
-    d_labels = []
-    d_bin_labels = []
-    d_features = []
-    for row in dev_data.iterrows():
-        part_id = str(int(row[1][0]))
-        features = row[1][1:].values
-        d_features.append(features)
-        label = dev_labels[part_id]['score']
-        b_label = dev_labels[part_id]['binary']
-        d_labels.append(label)
-        d_bin_labels.append(b_label)
+    # d_labels = []
+    # d_bin_labels = []
+    # d_features = []
+    # for row in dev_data.iterrows():
+    #     part_id = str(int(row[1][0]))
+    #     features = row[1][1:].values # Features will be off by one cell
+    #     d_features.append(features)
+    #     label = dev_labels[part_id]['score']
+    #     b_label = dev_labels[part_id]['binary']
+    #     d_labels.append(label)
+    #     d_bin_labels.append(b_label)
+
+
 
     # print 'Random binary baseline = ', float(d_bin_labels.count(0)) / len(d_bin_labels)
-
-
 
     audio_names =  train_data.columns[1:371]
     # video_names = train_data.columns[371: 556]
     video_names = train_data.columns[446:546] # Look at AUs only
     text_names = train_data.columns[556:]
+    syntax_names = train_data.columns[-85:]
+
+    # ----------------- EXPERIMENT: Evaluating Informed Early Fusion ----------------- #
+
+    # GET LABELS
+    train_csv = path+'Labels/training_split.csv'
+    train_df = pandas.read_csv(train_csv)
+    dev_csv = path+'Labels/dev_split.csv'
+    dev_df = pandas.read_csv(dev_csv)
+    labels = {}
+    for row in train_df.iterrows():
+        row = row[1]
+        labels[str(row['Participant_ID'])] = {'binary': row['PHQ_Binary'],
+                                                    'score': row['PHQ_Score'], 'gender': row['Gender']}
+    for row in dev_df.iterrows():
+        row = row[1]
+        labels[str(row['Participant_ID'])] = {'binary': row['PHQ_Binary'],
+                                                  'score': row['PHQ_Score'], 'gender': row['Gender']}
+
+    # Get informed features
+    informed_path = path+'Informed_Features/Informed_Features/'
+    informed_files = os.listdir(informed_path)[1:]
+    included_ids = []
+    informed_features = []
+    bin_labels_informed = []
+    count = 0
+    for i, f in enumerate(informed_files):
+        data = pandas.read_csv(informed_path+f)
+        features = data.iloc[0].values
+        my_id = f.replace('_informed.csv','')
+        if my_id in labels.keys():
+            count += 1
+            bin_label = labels[my_id]['binary']
+            bin_labels_informed.append(bin_label)
+            included_ids.append(my_id)
+            informed_features.append(features)
+
+    features = {}
+    for row in train_data.iterrows():
+        part_id = str(int(row[1][0]))
+        if part_id in included_ids:
+            feats = row[1][1:].values
+            features[part_id] = feats
+    for row in dev_data.iterrows():
+        part_id = str(int(row[1][0]))
+        if part_id in included_ids:
+            feats = row[1][1:].values
+            features[part_id] = feats
+
+    all_features = []
+    bin_labels = []
+    for ID in included_ids:
+        b_label = labels[ID]['binary']
+        bin_labels.append(b_label)
+        all_features.append(features[ID])
+
+
+    # Informed fusion
+    print('Informed fusion results...\n')
+    print Fusions.predict_class_cv(informed_features, bin_labels_informed)
 
     # Audio only
+    audio = [feats[:370] for feats in all_features]
     print('Results for audio only...\n')
-    audio_t = [feats[:370] for feats in t_features]
-    audio_d = [feats[:370] for feats in d_features]
-    # print len(audio_d[0])
-    # print Fusions.predict_regression(audio_t, audio_d,t_labels,d_labels)
-    # print Fusions.predict_class(audio_t,audio_d,t_bin_labels,d_bin_labels)
-    Fusions.upsample_data(audio_t, t_bin_labels, audio_names, audio_d, d_bin_labels)
+    print Fusions.predict_class_cv(audio, bin_labels)
+
+    # Syntax only
+    syntax = [feats[-85:] for feats in all_features]
+    print('Syntax results...\n')
+    print Fusions.predict_class_cv(syntax, bin_labels)
+
+    # Early fusion
+    early = []
+    for i, f in enumerate(audio):
+        early.append(audio[i].tolist() + syntax[i].tolist())
+    print('Early fusion results...\n')
+    print Fusions.predict_class_cv(early, bin_labels)
+
+
+    # Informed early fusion
 
     # Text only
-    print('Results for text only...\n')
-    text_t = [feats[555:] for feats in t_features]
-    text_d = [feats[555:] for feats in d_features]
+    # print('Results for text only...\n')
+    # text_t = [feats[555:] for feats in t_features]
+    # text_d = [feats[555:] for feats in d_features]
     # print len(text_t[0])
     # print Fusions.predict_regression(text_t,text_d,t_labels,d_labels)
     # print Fusions.predict_class(text_t,text_d,t_bin_labels,d_bin_labels)
-    Fusions.upsample_data(text_t, t_bin_labels, text_names, text_d, d_bin_labels)
+    # Fusions.upsample_data(text_t, t_bin_labels, text_names, text_d, d_bin_labels)
 
     # Video only
-    print('Results for video only...\n')
+    # print('Results for video only...\n')
     # video_t = [feats[370:555] for feats in t_features]
     # video_d = [feats[370:555] for feats in d_features]
     # Look at AUs only
-    video_t = [feats[445:545] for feats in t_features]
+    # video_t = [feats[445:545] for feats in t_features]
     # print "Number of features", len(video_t[0])
-    video_d = [feats[445:545] for feats in d_features]
+    # video_d = [feats[445:545] for feats in d_features]
     # print len(video_d[0])
     # print Fusions.predict_regression(video_t, video_d, t_labels,d_labels)
     # print Fusions.predict_class(video_t, video_d, t_bin_labels,d_bin_labels)
 
     # Try upsampling on each unimodal dataset
-    Fusions.upsample_data(video_t, t_bin_labels, video_names, video_d, d_bin_labels)
+    # Fusions.upsample_data(video_t, t_bin_labels, video_names, video_d, d_bin_labels)
 
     #
     # # Early fusion
