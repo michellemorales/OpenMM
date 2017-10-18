59fb90f7252f3aa2ad18676f4fd42c53695d5b84
morales <mmorales@gradcenter.cuny.edu>
2017-10-18 07:55:53 -0400

    created a script to run a gui

--- scripts/gui.py
+++ scripts/gui.py
@@ -10,15 +10,41 @@ Created on Thurs Oct 05 07:36:55 2017
 #!/usr/bin/python
 import sys
 import os
-import Tkinter
+from Tkinter import *
 import FeatureExtract
+import json
 import tkMessageBox
-top=Tkinter.Tk()
-top.title("OpenMM")
+
 
 def run():
+    global pars
+    ibm_pass = str(pars["IBM_PASSWORD"])
+    ibm_un = str(pars["IBM_USERNAME"])
+    openface = str(pars["OPENFACE"])
+    # TODO:perform video analysis
+    print "Running OpenFace..."
+    FeatureExtract.extract_visual("/Users/morales/GitHub/OpenMM/examples/FerrisBuellerClip.mp4", openface)
+    # Perform audio analysis
+    print "Running Covarep..."
+    # FeatureExtract.extract_audio("/Users/morales/GitHub/OpenMM/examples/")
+    # Perform speech-to-text
+    print "Running speech-to-text..."
+    FeatureExtract.ibm_speech2text("/Users/morales/GitHub/OpenMM/examples/FerrisBuellerClip.wav", 'en-US', ibm_un , ibm_pass)
+    # Perform linguistic analysis
+    # Output prediction
 
 
-B=Tkinter.Button(top,text="Run OpenMM",command= helloCallBack)
-B.pack()
-top.mainloop()
+# Get config parameters
+config = "/Users/morales/Desktop/config.json"
+json_file = open(config, "r").read()
+pars = json.loads(json_file)
+# Make pars global
+
+win = Tk()
+win.title("OpenMM")
+L = Label(win, text="Please choose a video:")
+B = Button(win,text="Run OpenMM", command= run)
+L.pack()
+B.pack(padx=50, pady=50)
+win.mainloop()
+
