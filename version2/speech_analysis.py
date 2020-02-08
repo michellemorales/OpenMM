c66d8c2a03278cf17082a666d3da8ff44529bade
Michelle Morales <ms.morales3@gmail.com>
2020-02-08 15:53:10 -0500

    Function to run speech to text

--- version2/speech_analysis.py
+++ version2/speech_analysis.py
@@ -1,24 +1,20 @@
 import speech_recognition as sr
 
-AUDIO_FILE = "../Examples/FerrisBuellerClip.wav"
 
-
-def speech_recognizer():
+def speech_recognizer(audio):
     # use the audio file as the audio source
     r = sr.Recognizer()
-    with sr.AudioFile(AUDIO_FILE) as source:
-        audio = r.record(source)  # read the entire audio file
+    with sr.AudioFile(audio) as source:
+        read_audio = r.record(source)  # read the entire audio file
 
     # recognize speech using Sphinx
     try:
-        transcript = r.recognize_sphinx(audio)
+        transcript = r.recognize_sphinx(read_audio)
         print("Sphinx thinks you said " + transcript)
-        with open("transcript.txt", 'w') as out_file:
+        transcript_file = audio.replace('.wav', '_transcript.txt')
+        with open(transcript_file, 'w') as out_file:
             out_file.write(transcript)
     except sr.UnknownValueError:
         print("Sphinx could not understand audio")
     except sr.RequestError as e:
         print("Sphinx error; {0}".format(e))
-
-
-# speech_recognizer()
