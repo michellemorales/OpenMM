3da96345dc1e9dd560a78566206d0f43e33884cb
Michelle Morales <ms.morales3@gmail.com>
2020-02-08 15:39:52 -0500

    Updated functions for running OpenFace and converting video to audio.

--- version2/video_analysis.py
+++ version2/video_analysis.py
@@ -1,9 +1,24 @@
 import subprocess
+import os.path
+import moviepy.editor as mp
 
-def extract_visual(video, openface):
+
+def extract_visual(video):
     # Extracts visual features using OpenFace, requires the OpenFace () repo to be installed
-    csv = video.replace('.mp4','_openface.csv')
-    print('Launching OpenFace to extract visual features... \n\n\n\n\n')
-    command = '%s -f %s -of %s'%(openface, video, csv)
-    subprocess.call(command, shell=True)
-    print 'DONE! Visual features saved to %s' % csv
+    csv = video.replace('.mp4', '_openface.csv')
+    if os.path.isfile(video):
+        try:
+            command = '{openface_path} -f {input_file} -of {output_file}'.format(openface_path='OpenFace/build/bin/FeatureExtraction', input_file=video, output_file=csv)
+            subprocess.call(command, shell=True)
+            print('Success! Visual features saved to {csv}'.format(csv=csv))
+        except:
+            print('ERROR: Unable to run OpenFace.')
+    else:
+        print("Video file does not exist.")
+
+
+def video2audio(video):
+    # Converts video to audio
+    audio_file = video.replace('.mp4', '.mp3')
+    clip = mp.VideoFileClip(video).subclip(0, 20)
+    clip.audio.write_audiofile(audio_file)
